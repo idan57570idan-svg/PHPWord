ef8075f74594ed9d11d37945fc3aec9a969e471a
idan57570idan-svg <idan57570idan@gmail.com>
2026-05-20 01:29:13 +0300

    Refactor ImageTest by removing redundant comments
    Removed unnecessary comments and improved code readability in ImageTest.php.

--- tests/PhpWordTests/Element/ImageTest.php
+++ tests/PhpWordTests/Element/ImageTest.php
@@ -22,14 +22,8 @@ use PhpOffice\PhpWord\Element\Image;
 use PhpOffice\PhpWord\SimpleType\Jc;
 use PhpOffice\PhpWordTests\AbstractWebServerEmbedded;
 
-/**
- * Test class for PhpOffice\PhpWord\Element\Image.
- */
 class ImageTest extends AbstractWebServerEmbedded
 {
-    /**
-     * New instance.
-     */
     public function testConstruct(): void
     {
         $src = __DIR__ . '/../_files/images/firefox.png';
@@ -43,9 +37,6 @@ class ImageTest extends AbstractWebServerEmbedded
         self::assertInstanceOf('PhpOffice\\PhpWord\\Style\\Image', $oImage->getStyle());
     }
 
-    /**
-     * New instance with style.
-     */
     public function testConstructWithStyle(): void
     {
         $src = __DIR__ . '/../_files/images/firefox.png';
@@ -62,27 +53,25 @@ class ImageTest extends AbstractWebServerEmbedded
         self::assertInstanceOf('PhpOffice\\PhpWord\\Style\\Image', $oImage->getStyle());
     }
 
-    /**
-     * Valid image types.
-     *
-     * @dataProvider providerImages
-     */
     public function testImages($source, $type, $extension, $createFunction, $imageFunction, $imageQuality): void
     {
         $nam = ucfirst((string) strtok($source, '.'));
         $source = __DIR__ . "/../_files/images/{$source}";
         $image = new Image($source, null, null, $nam);
+
         self::assertEquals($source, $image->getSource());
         self::assertEquals($nam, $image->getName());
         self::assertEquals(md5($source), $image->getMediaId());
         self::assertEquals($type, $image->getImageType());
         self::assertEquals($extension, $image->getImageExtension());
         self::assertEquals($createFunction, $image->getImageCreateFunction());
+
         if ($imageFunction) {
             self::assertNotNull($image->getImageFunction());
         } else {
             self::assertNull($image->getImageFunction());
         }
+
         self::assertEquals($imageQuality, $image->getImageQuality());
         self::assertFalse($image->isMemImage());
         self::assertNotNull($image->getImageStringData());
@@ -99,9 +88,6 @@ class ImageTest extends AbstractWebServerEmbedded
         ];
     }
 
-    /**
-     * Get style.
-     */
     public function testStyle(): void
     {
         $oImage = new Image(
@@ -112,28 +98,21 @@ class ImageTest extends AbstractWebServerEmbedded
         self::assertInstanceOf('PhpOffice\\PhpWord\\Style\\Image', $oImage->getStyle());
     }
 
-    /**
-     * Get alt text.
-     */
-    public function testAltText(): void {
+    public function testAltText(): void
+    {
         $source = __DIR__ . '/../_files/images/earth.jpg';
         $altText = 'Picture of the earth from space';
         $image = new Image($source, null, false, null, $altText);
+
         self::assertEquals($altText, $image->getAltText());
     }
 
-    /**
-     * Test invalid local image.
-     */
     public function testInvalidImageLocal(): void
     {
         $this->expectException(\PhpOffice\PhpWord\Exception\InvalidImageException::class);
         new Image(__DIR__ . '/../_files/images/thisisnotarealimage');
     }
 
-    /**
-     * Test invalid PHP Image.
-     */
     public function testInvalidImagePhp(): void
     {
         $this->expectException(\PhpOffice\PhpWord\Exception\InvalidImageException::class);
@@ -141,49 +120,41 @@ class ImageTest extends AbstractWebServerEmbedded
         $source = $object->getSource();
     }
 
-    /**
-     * Test unsupported image.
-     */
     public function testUnsupportedImage(): void
     {
         $this->expectException(\PhpOffice\PhpWord\Exception\UnsupportedImageTypeException::class);
-        //disable ssl verification, never do this in real application, you should pass the certificiate instead!!!
+
         $arrContextOptions = [
             'ssl' => [
                 'verify_peer' => false,
                 'verify_peer_name' => false,
             ],
         ];
+
         stream_context_set_default($arrContextOptions);
         $object = new Image(self::getRemoteBmpImageUrl());
         $source = $object->getSource();
     }
 
-    /**
-     * Get relation Id.
-     */
     public function testRelationID(): void
     {
         $oImage = new Image(__DIR__ . '/../_files/images/earth.jpg', ['width' => 100]);
         $iVal = mt_rand(1, 1000);
+
         $oImage->setRelationId($iVal);
         self::assertEquals($iVal, $oImage->getRelationId());
     }
 
-    /**
-     * Test archived image.
-     */
     public function testArchivedImage(): void
     {
         $archiveFile = __DIR__ . '/../_files/documents/reader.docx';
         $imageFile = 'word/media/image1.jpeg';
+
         $image = new Image("zip://{$archiveFile}#{$imageFile}");
+
         self::assertEquals('image/jpeg', $image->getImageType());
     }
 
-    /**
-     * Test getting image as string.
-     */
     public function testImageAsStringFromFile(): void
     {
         $image = new Image(__DIR__ . '/../_files/images/earth.jpg');
@@ -192,27 +163,23 @@ class ImageTest extends AbstractWebServerEmbedded
         self::assertNotNull($image->getImageStringData(true));
     }
 
-    /**
-     * Test getting image from zip as string.
-     */
     public function testImageAsStringFromZip(): void
     {
         $archiveFile = __DIR__ . '/../_files/documents/reader.docx';
         $imageFile = 'word/media/image1.jpeg';
+
         $image = new Image("zip://{$archiveFile}#{$imageFile}");
 
         self::assertNotNull($image->getImageStringData());
         self::assertNotNull($image->getImageStringData(true));
     }
 
-    /**
-     * Test construct from string.
-     */
     public function testConstructFromString(): void
     {
         $source = file_get_contents(__DIR__ . '/../_files/images/earth.jpg');
 
         $image = new Image($source);
+
         self::assertEquals($source, $image->getSource());
         self::assertEquals(md5((string) $source), $image->getMediaId());
         self::assertEquals('image/jpeg', $image->getImageType());
@@ -226,14 +193,12 @@ class ImageTest extends AbstractWebServerEmbedded
         self::assertNotNull($image->getImageStringData(true));
     }
 
-    /**
-     * Test construct from GD.
-     */
     public function testConstructFromGd(): void
     {
         $source = self::getRemoteImageUrl();
 
         $image = new Image($source);
+
         self::assertEquals($source, $image->getSource());
         self::assertEquals(md5($source), $image->getMediaId());
         self::assertEquals('image/png', $image->getImageType());
@@ -247,13 +212,9 @@ class ImageTest extends AbstractWebServerEmbedded
         self::assertNotNull($image->getImageStringData(true));
     }
 
-    /**
-     * Test invalid string image.
-     */
     public function testInvalidImageString(): void
     {
         $this->expectException(\PhpOffice\PhpWord\Exception\InvalidImageException::class);
-        $object = new Image('this_is-a_non_valid_image');
-        $source = $object->getSource();
+        new Image('this_is-a_non_valid_image');
     }
 }
